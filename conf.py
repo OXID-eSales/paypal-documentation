a4f5df749b15eaefd5081c615bd4007ce0589425
Juergen-Busch <juergen.busch@oxid-esales.com>
2020-08-03 10:44:34 +0200

    Initial commit PayPal 6.2

--- conf.py
+++ conf.py
@@ -55,7 +55,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'PayPal'
-copyright = '2019 - {}, OXID eSales AG'.format(dt.date.today().year)
+copyright = '{}, OXID eSales AG'.format(dt.date.today().year)
 author = 'OXID eSales AG'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -63,7 +63,7 @@ author = 'OXID eSales AG'
 # built documents.
 #
 # The short X.Y version.
-version = '6.1'
+version = '6.2'
 # The full version, including alpha/beta/rc tags.
 release = '6'
 
@@ -128,15 +128,14 @@ todo_include_todos = True
 html_theme = 'sphinx_rtd_theme'
 
 html_context = {
-    'current_version': '6.1',
+    'current_version': '6.2',
     'versions':
-        [('5.2', 'https://docs.oxid-esales.com/modules/paypal/en/5.2/'),
-         ('5.3', 'https://docs.oxid-esales.com/modules/paypal/en/5.3/'),
-         ('6.0', 'https://docs.oxid-esales.com/modules/paypal/en/6.0/'),
-         ('6.1', 'https://docs.oxid-esales.com/modules/paypal/en/6.1/')],
+        [('6.0', 'https://docs.oxid-esales.com/modules/paypal/en/6.0/'),
+         ('6.1', 'https://docs.oxid-esales.com/modules/paypal/en/6.1/'),
+         ('6.2', 'https://docs.oxid-esales.com/modules/paypal/en/6.2/')],
     'languages':
-        [('EN', 'https://docs.oxid-esales.com/modules/paypal/en/6.1/'),
-         ('DE', 'https://docs.oxid-esales.com/modules/paypal/de/6.1/')],
+        [('EN', 'https://docs.oxid-esales.com/modules/paypal/en/6.2/'),
+         ('DE', 'https://docs.oxid-esales.com/modules/paypal/de/6.2/')],
     'css_files': [('_static/oxid.css')],
     'theme_logo_only': True,
     'show_sphinx': False,
@@ -145,7 +144,7 @@ html_context = {
     # Format Template: https://{{ github_host|default("github.com") }}/{{ github_user }}/{{ github_repo }}/blob/{{github_version }}{{ conf_py_path }}{{ pagename }}{{ suffix }}
     'github_user': 'OXID-eSales',
     'github_repo': 'paypal-documentation',
-    'github_version': '6.1-en/'
+    'github_version': '6.2-en/'
 }
 
 # Theme options are theme-specific and customize the look and feel of a theme
