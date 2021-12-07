c32b8b66c766eff89133edb7f6aef80d8f2edae8
christoph albrecht <christoph.albrecht@oxid-esales.com>
2021-12-07 16:23:18 +0100

    Version switch adjusted to 6.4

--- conf.py
+++ conf.py
@@ -133,7 +133,8 @@ html_context = {
         [('6.0', 'https://docs.oxid-esales.com/modules/paypal/en/6.0/'),
          ('6.1', 'https://docs.oxid-esales.com/modules/paypal/en/6.1/'),
          ('6.2', 'https://docs.oxid-esales.com/modules/paypal/en/6.2/'),
-         ('6.3', 'https://docs.oxid-esales.com/modules/paypal/en/6.3/')],
+         ('6.3', 'https://docs.oxid-esales.com/modules/paypal/en/6.3/'),
+         ('6.4', 'https://docs.oxid-esales.com/modules/paypal/en/6.4/')],
     'languages':
         [('EN', 'https://docs.oxid-esales.com/modules/paypal/en/6.1/'),
          ('DE', 'https://docs.oxid-esales.com/modules/paypal/de/6.1/')],
